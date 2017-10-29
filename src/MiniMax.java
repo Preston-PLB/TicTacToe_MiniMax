0a771585e72391d25f6401fe5620348f45204bcd
Preston Baxter <baxterboyz1@gmail.com>
2017-10-29 13:01:49 -0500

    First version of minimax function. Also added isFull()

--- src/MiniMax.java
+++ src/MiniMax.java
@@ -1,14 +1,51 @@
 public class MiniMax {
 
-    //thing
+    public int minimax(int[][] board, boolean maximizer){
+        if(isFull(board)){
+            return evaluate(board);
+        }
 
-    /*
-    * @param board: a 3x3 2d array with values -1, 0, 1. 1 being maximizer, 0 being empty, -1 being minimizer.
+        if(maximizer) {
+            int bestScore = Integer.MIN_VALUE;
+            for (int y = 0; y < 3; y++) {
+                for (int x = 0; x < 3; x++) {
+                    if (board[y][x] == 0) {
+                        int[][] temp = board;
+                        temp[y][x] = 1;
+                        int score = minimax(temp, !maximizer);
+                        if(score > bestScore){
+                            bestScore = score;
+                        }
+                        return bestScore;
+                    }
+                }
+            }
+        }else{
+            int bestScore = Integer.MAX_VALUE;
+            for (int y = 0; y < 3; y++) {
+                for (int x = 0; x < 3; x++) {
+                    if (board[y][x] == 0) {
+                        int[][] temp = board;
+                        temp[y][x] = -1;
+                        int score = minimax(temp, !maximizer);
+                        if(score < bestScore){
+                            bestScore = score;
+                        }
+                        return bestScore;
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    /**
+    * @param board a 3x3 2d array with values -1, 0, 1. 1 being maximizer, 0 being empty, -1 being minimizer.
     *
     * @return return arbitrary integer. if positive then maximizer wins, if negative minimizer wins, if 0 either no one has won or stalemate.
     *
     * */
-    public int evaluate(int[][] board){
+    private int evaluate(int[][] board){
         //check rows
         for(int y = 0; y<3; y++){
             int sum = board[y][0]+board[y][1]+board[y][2];
@@ -46,6 +83,21 @@ public class MiniMax {
 
 
         return 0;
+    }
+
+    /**
+     * @param board a 3x3 2d array with values -1, 0, 1
+     *
+     * @return true if the board is full of 1's or/and -1's. False if there are 0's (there are empty spots).
+     */
+    private boolean isFull(int[][] board){
+        for(int y = 0; y<3; y++){
+           for(int x = 0; x<3; x++){
+               //yes this is syntactically valid
+               if(board[y][x] == 0) return false;
+           }
+        }
+        return true;
     }
 
 }
